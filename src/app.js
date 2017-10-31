72ffb602b801387e8ed82936317782df9e79a45a
Jesse Tomchak <jtomchak@gmail.com>
2017-10-31 15:14:36 -0700

    images are showing up on the page

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@ import $ from "jquery";
 import "./style.scss";
 
 $(document).ready(function() {
-  const hello = () => console.log("linked and working....");
-  hello();
+  let moviesList = [];
 
   const getMovies = () => {
     const movieURL =
@@ -11,11 +10,38 @@ $(document).ready(function() {
     fetch(movieURL)
       .then(res => res.json())
       .then(payload => {
-        console.log(payload);
+        moviesList = payload.results;
+        console.log(payload.results);
+        createPosters(); //call create now that we have data.
       })
       .catch(err => {
         console.error(err);
       });
   };
+  const createPosters = () => {
+    for (let i = 0; i < moviesList.length; i++) {
+      let movie = moviesList[i];
+      var divColumn = $("<div>").attr("class", "col-md-4");
+      var divVisibleSpaceMD = $("<div>").attr(
+        "class",
+        "clearfix visible-md-block"
+      );
+      var divThumbnail = $("<div>")
+        .attr("class", "poster_thumbnail")
+        .append(
+          $("<img>")
+            .attr("src", "https://image.tmdb.org/t/p/w500/" + movie.poster_path)
+            .attr("class", "poster_image")
+        );
+      divColumn.append(divThumbnail); //put image in div
+      $(".movies").append(divColumn); //put div in movies class div
+      if (i && (i + 1) % 3 === 0) {
+        $(".movies").append(divVisibleSpaceMD); //every 3rd add a clearfix
+      }
+    }
+  };
   getMovies();
+
+  //Take Poster URL's and create a img tag from each one,
+  //gonna need to be it's own box, that box will have col-md-4
 });
