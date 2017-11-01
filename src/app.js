9e62d1570af7e0f32fb1bfd6e87c82bdd88aa88c
Jesse Tomchak <jtomchak@gmail.com>
2017-11-01 09:22:30 -0700

    WIP

--- src/app.js
+++ src/app.js
@@ -3,6 +3,30 @@ import "./style.scss";
 
 $(document).ready(function() {
   let moviesList = [];
+  // When page loads...:
+  // $("div.content div").hide(); // Hide all content
+  $("nav li:first")
+    .addClass("current")
+    .show(); // Activate first tab
+  $("#movies").show(); // Show first tab content
+  $("#surprise").show(); // Show first tab content
+
+  $(".navMovies").click(function(event) {
+    $("#movies").show();
+    $("#surprise").hide();
+  });
+  $(".navSurprise").click(function(event) {
+    $("#surprise").show();
+    $("#movies").hide();
+  });
+
+  // On Click Event (within list-element!)
+  $("nav li").click(function() {
+    $("nav li").removeClass("active"); // Remove any active class
+    $(this).addClass("active"); // Add "current" class to selected tab
+
+    $("div.content div").hide(); // Hide all content
+  }); // end click method;
 
   const getMovies = () => {
     const movieURL =
@@ -19,29 +43,76 @@ $(document).ready(function() {
       });
   };
   const createPosters = () => {
-    for (let i = 0; i < moviesList.length; i++) {
-      let movie = moviesList[i];
-      var divColumn = $("<div>").attr("class", "col-md-4");
-      var divVisibleSpaceMD = $("<div>").attr(
-        "class",
-        "clearfix visible-md-block"
-      );
-      var divThumbnail = $("<div>")
-        .attr("class", "poster_thumbnail")
-        .append(
-          $("<img>")
-            .attr("src", "https://image.tmdb.org/t/p/w500/" + movie.poster_path)
-            .attr("class", "poster_image")
+    const movieElements = moviesList
+      .map(function(movie) {
+        var divColumn = $("<div>").attr("class", "col-sm-6 col-md-4, col-lg-4");
+        var divThumbnail = $("<div>")
+          .attr("class", "thumbnail poster_thumbnail")
+          .append(
+            $("<img>")
+              .attr(
+                "src",
+                "https://image.tmdb.org/t/p/w500/" + movie.poster_path
+              )
+              .attr("class", "poster_image")
+          );
+        divThumbnail.append(
+          $("<div>")
+            .attr("class", "caption")
+            .append(
+              $("<h2>")
+                .append(movie.title)
+                .attr("class", "hidden-xs hidden-sm")
+            )
+            .append(
+              $("<h4>")
+                .append(movie.release_date)
+                .attr("class", "hidden-xs hidden-sm")
+            )
+            .append(
+              $("<p>", {
+                class: "",
+                text: movie.overview
+              }).attr("class", "hidden-xs hidden-sm")
+            )
+            .append(
+              $("<p>").append(
+                $("<a>")
+                  .attr("href", "http://www.imdb.com/title/")
+                  .attr("class", "btn btn-primary")
+                  .attr("role", "button")
+                  .text("Details")
+              )
+            )
         );
-      divColumn.append(divThumbnail); //put image in div
-      $(".movies").append(divColumn); //put div in movies class div
-      if (i && (i + 1) % 3 === 0) {
-        $(".movies").append(divVisibleSpaceMD); //every 3rd add a clearfix
-      }
+        divColumn.append(divThumbnail); //put image in div
+        return divColumn;
+      })
+      .map(appendElementWithVisibleSpacing);
+  };
+
+  //appending movie elements to the dom with clearfix spacing for md, sm, lg
+  const appendElementWithVisibleSpacing = (movieElement, index) => {
+    var divVisibleSpaceMDLG = $("<div>").attr(
+      "class",
+      "clearfix visible-md-block visible-lg-block"
+    );
+    var divVisibleSpaceSM = $("<div>").attr(
+      "class",
+      "clearfix visible-sm-block"
+    );
+    //put div in movies class div
+    $(".movies").append(movieElement);
+    //every 3rd add a clearfix for md
+    if (index && (index + 1) % 3 === 0) {
+      $(".movies").append(divVisibleSpaceMDLG);
+    }
+    //adding spacing for every 2nd for sm-6, 2 across for sm
+    if (index && (index + 1) % 2 === 0) {
+      $(".movies").append(divVisibleSpaceSM);
     }
   };
-  getMovies();
 
-  //Take Poster URL's and create a img tag from each one,
-  //gonna need to be it's own box, that box will have col-md-4
+  //get movies onload
+  getMovies();
 });
